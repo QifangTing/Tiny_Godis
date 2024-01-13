79bf545d6ea59a100a24aee7325ee9cbda20ff4b
QifangTing <2573557410@qq.com>
2024-01-13 21:19:05 +0800

    Imple String

--- database/string.go
+++ database/string.go
@@ -1,11 +1,89 @@
 package database
 
+import (
+	"Tiny_Godis/interface/database"
+	"Tiny_Godis/interface/resp"
+	"Tiny_Godis/resp/reply"
+)
+
 // execGet：get k1
+func execGet(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	bytes, err := db.getAsString(key)
+	if err != nil {
+		return err
+	}
+	if bytes == nil {
+		return &reply.NullBulkReply{}
+	}
+	return reply.MakeBulkReply(bytes)
+}
+
+func (db *DB) getAsString(key string) ([]byte, reply.ErrorReply) {
+	entity, ok := db.GetEntity(key)
+	if !ok {
+		return nil, nil
+	}
+	bytes, ok := entity.Data.([]byte)
+	if !ok {
+		return nil, &reply.WrongTypeErrReply{}
+	}
+	return bytes, nil
+}
 
 // execSet：set k v
+func execSet(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	value := args[1]
+	entity := &database.DataEntity{ // 新构造一个entity，存入
+		Data: value,
+	}
+	db.PutEntity(key, entity)
+	return &reply.OkReply{}
+}
 
 // execSetNX：setnex k v
+func execSetNX(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	value := args[1]
+	entity := &database.DataEntity{
+		Data: value,
+	}
+	result := db.PutIfAbsent(key, entity) // 返回值相应变化
+	return reply.MakeIntReply(int64(result))
+}
 
 // execGetSet：getset k v 返回旧值
+func execGetSet(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	value := args[1]
+	entity, exists := db.GetEntity(key)
+
+	db.PutEntity(key, &database.DataEntity{Data: value})
+
+	if !exists {
+		return reply.MakeNullBulkReply()
+	}
+	old := entity.Data.([]byte)
+	return reply.MakeBulkReply(old)
+}
 
 // execStrLen：strlen k
+func execStrLen(db *DB, args [][]byte) resp.Reply {
+	key := string(args[0])
+	entity, exists := db.GetEntity(key)
+	if !exists {
+		return reply.MakeNullBulkReply()
+	} else {
+		res := entity.Data.([]byte)
+		return reply.MakeIntReply(int64(len(res)))
+	}
+}
+
+func init() {
+	RegisterCommand("Get", execGet, 2)
+	RegisterCommand("Set", execSet, -3)
+	RegisterCommand("SetNx", execSetNX, 3)
+	RegisterCommand("GetSet", execGetSet, 3)
+	RegisterCommand("StrLen", execStrLen, 2)
+}
